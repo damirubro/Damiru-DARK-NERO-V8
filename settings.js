fbe2b5df7c2f4788532226d1edb47c6684cded5e
damirubro <95349271+damirubro@users.noreply.github.com>
2023-03-25 18:24:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = "MXhTeHdLWUQ=" //session Id
+global.session = "QzlhckR1alg=" //session Id
 global.ownernomer = "94770475213" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['94770475213'] //ur premium number
